0ba6e2a6fb6377b330414929be37317a3b7bc71b
nao20010128nao <nao20010128@gmail.com>
2018-06-25 22:03:14 +0900

    開放

--- src/main/kotlin/com/nao20010128nao/Cryptorage/ServerExposed.kt
+++ src/main/kotlin/com/nao20010128nao/Cryptorage/ServerExposed.kt
@@ -1,2 +1,6 @@
 package com.nao20010128nao.Cryptorage
 
+import com.nao20010128nao.Cryptorage.internal.CryptorageServer
+import net.freeutils.httpserver.HTTPServer
+
+fun Cryptorage.asHttpServer(port: Int = 80, indexHtmlAsRoot: Boolean, supportPartial: Boolean): HTTPServer = CryptorageServer(port, this, indexHtmlAsRoot, supportPartial)
